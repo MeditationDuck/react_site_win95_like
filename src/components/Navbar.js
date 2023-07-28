893656147091a8481bc9303b9946c622fe30d143
yoshinao <yoshinao@fit.cvut.cz>
2023-07-28 22:31:25 +0900

    feat: add contact links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
-// import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
+
+
+// icons from https://icon-icons.com/icon/whatsapp-pixel-logo/181923 cc license
 
 function Navbar() {
   const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
-  const [buttonStatus, setButtonStatus] = useState({ start: false, search1: false, search2: false });
+  const [buttonStatus, setButtonStatus] = useState({ 
+    start: false, 
+    search: false, 
+    instagram: false,
+  });
+
+  const bar_contents = [
+    {name: 'Start', icon: 'windows.ico', link: null},
+    // {name: 'Search', icon: null},
+    {name: 'Instagram', icon: 'instagram.ico', link: 'https://www.instagram.com/meditationduck/'},
+    {name: 'Youtube', icon: 'youtube.ico', link: 'https://www.youtube.com/channel/UCZ3e0b37MykMtMUQQ-7Y4dA'},
+    {name: 'Twitter', icon: 'twitter.ico', link: 'https://twitter.com/meditationduck'},
+    {name: 'Facebook', icon: 'facebook.ico', link: 'https://www.facebook.com/naoki.yoshida.796774/'},
+  ];
   
   useEffect(() => {
     const interval = setInterval(() => {
@@ -15,21 +30,22 @@ function Navbar() {
   // const handleClick = (button) => {
   //   setButtonStatus(prev => ({...prev, [button]: !prev[button]}));
   // }
-  const bar_contents = [
-    {name: 'Start', icon: 'windows.ico'},
-    {name: 'Search', icon: null},
-    {name: 'Instagram', icon: 'instagram.ico'},
-  ];
+ 
   
   return(
-    <div className='bg-element w-full flex flex-row justify-between items-center fixed bottom-0 py-2 px-2 z-10 text-black'>
+    <div className='bg-element w-full flex justify-between items-center fixed bottom-0 py-1 px-2 z-1 text-black'>
       {bar_contents.map((content, index) => {
         console.log(process.env.PUBLIC_URL + content.icon)
         return(
         <div 
-        class={`flex p-1 cursor-pointer flex-shrink-0 m-1 ${buttonStatus.start ? 'custom-border-pushed' : 'custom-border'}`}
-        onMouseDown={() => setButtonStatus(prev => ({...prev, start: true}))}
-        onMouseUp={() => setButtonStatus(prev => ({...prev, start: false}))}
+        class={`flex p-1 cursor-pointer flex-shrink-0 mx-1 ${buttonStatus[content.name.toLowerCase()] ? 'custom-border-pushed' : 'custom-border'}`}
+        onMouseDown={() => setButtonStatus(prev => ({...prev, [content.name.toLowerCase()]: true}))}
+        onMouseUp={() => {
+          setButtonStatus(prev => ({...prev, [content.name.toLowerCase()]: false}));
+          if(content.link !== null) {
+            window.location.href = content.link;
+          }
+          }}
         >
         { content.icon &&
           <img 
@@ -43,16 +59,14 @@ function Navbar() {
       )
       })}
       
-    
-      <div className="w-full">
-      </div>
+      <div className="w-full" />
 
-
-      <div className='custom-border-pushed flex py-1 px-2'>
+      <div 
+      className='custom-border-pushed py-1 px-2'
+      style={{ whiteSpace: 'nowrap'}}
+      >
         <p>{time}</p>
       </div>
-
-
     </div>
   )
 }
